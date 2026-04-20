24831d042a4015803227f11e4c4ea1e85857dcef
NIX VAIL <vasilishenanika@gmail.com>
2026-04-20 20:26:33 +0300

    obsidian

---
+++ OBSDNIX/18. PROGRAMMING/18. CS50 PYTHON/abiteofpython.py
@@ -0,0 +1,16 @@
+# 31/01/26
+
+def op(a,b):
+    if a > b:
+        print("max", a)
+    elif a == b:
+        print( a = b)
+    else:
+        print("max", b)
+
+op(6,7)
+
+x = 5
+y = 90
+
+op(x,y)
